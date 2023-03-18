c42c3cf0da97c105f01bce1d9bb059c139314930
Marko Mladenovic <marko.mladenovic@fl3xx.com>
2023-03-19 00:00:00 +0100

    adding legend and tree icons

---
+++ src/main/java/CustomIconRenderer.java
@@ -0,0 +1,79 @@
+import burp.api.montoya.logging.Logging;
+import utils.UrlUtils;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import java.awt.*;
+
+public class CustomIconRenderer extends DefaultTreeCellRenderer {
+
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+        super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
+
+        Object nodeObj = ((DefaultMutableTreeNode) value).getUserObject();
+        DiggerNode diggerNode = (DiggerNode) nodeObj;
+        UrlUtils.HttpResponseCodeStatus responseStatus = diggerNode.getResponseStatus();
+
+        switch (responseStatus) {
+            case INFORMATIONAL:
+                if (leaf) {
+                    setIcon(new ImageIcon(CustomIconRenderer.class.getResource("/home/marko/DirDigger/src/main/resources/images/info-document.png")));
+                } else {
+                    if (expanded) {
+                        setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/info-dir-open.png")));
+                    } else {
+                        setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/info-dir-closed.png")));
+                    }
+                }
+                break;
+            case SUCCESS:
+                if (leaf) {
+                    setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/success-document.png")));
+                } else {
+                    if (expanded) {
+                        setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/success-dir-open.png")));
+                    } else {
+                        setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/success-dir-closed.png")));
+                    }
+                }
+                break;
+            case REDIRECTION:
+                if (leaf) {
+                    setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/redirect-document.png")));
+                } else {
+                    if (expanded) {
+                        setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/redirect-dir-open.png")));
+                    } else {
+                        setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/redirect-dir-closed.png")));
+                    }
+                }
+                break;
+            case CLIENT_ERROR:
+                if (leaf) {
+                    setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/client_error-document.png")));
+                } else {
+                    if (expanded) {
+                        setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/client_error-dir-open.png")));
+                    } else {
+                        setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/client_error-dir-closed.png")));
+                    }
+                }
+                break;
+            default:
+                if (leaf) {
+                    setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/server_error-document.png")));
+                } else {
+                    if (expanded) {
+                        setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/server_error-dir-open.png")));
+                    } else {
+                        setIcon(new ImageIcon(CustomIconRenderer.class.getResource("images/server_error-dir-closed.png")));
+                    }
+                }
+
+        }
+
+        return this;
+    }
+}
--- src/main/java/DiggerNode.java
+++ src/main/java/DiggerNode.java
@@ -1,13 +1,24 @@
+import utils.UrlUtils;
+
+import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 public class DiggerNode {
 
     private DefaultMutableTreeNode parent;
     private String url;
+    private UrlUtils.HttpResponseCodeStatus responseStatus;
+    private String icon;
 
-    public DiggerNode(DefaultMutableTreeNode parent, String url) {
+    public DiggerNode(DefaultMutableTreeNode parent, String url, UrlUtils.HttpResponseCodeStatus responseStatus) {
         this.parent = parent;
         this.url = url;
+        this.responseStatus = responseStatus;
+    }
+
+    @Override
+    public String toString() {
+        return getUrl();
     }
 
     public DefaultMutableTreeNode getParent() {
@@ -24,5 +35,21 @@ public class DiggerNode {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public UrlUtils.HttpResponseCodeStatus getResponseStatus() {
+        return responseStatus;
+    }
+
+    public void setResponseStatus(UrlUtils.HttpResponseCodeStatus responseStatus) {
+        this.responseStatus = responseStatus;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
     }
 }
